edd7f4473526eb2e2c74b9107c0312fbdc00e8e1
brian tarqui <tarquibrian@gmail.com>
2023-04-11 17:53:00 -0400

    updated

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import logo from "../../images/logo.png";
+import Image from "next/image";
+import ToggleIcon from "../icons/toggle";
 
 const Header = () => {
   return (
-    <header>
-      <nav>Header</nav>
+    <header className="header__section">
+      <nav className="nav__container">
+        <div className="nav__container-logo">
+          <Image src={logo} alt="image logo" className=""></Image>
+          <span className="">News Website</span>
+        </div>
+        <div className="nav__container-options">
+          <span className="contact">Contact Now</span>
+          <span className="icon">
+            <ToggleIcon />
+          </span>
+          <span className="menu">Circle</span>
+        </div>
+      </nav>
     </header>
   );
 };
