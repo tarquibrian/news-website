5a554a3f877b59c17ebb1556e516a410bd5f109f
brian tarqui <tarquibrian@gmail.com>
2023-04-11 23:22:23 -0400

    test

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import artibleImg from "../../images/article1.jpg";
+import Image from "next/image";
 
 const Hero = () => {
-  return <section>Hero</section>;
+  return (
+    <article>
+      <div className="header__container">
+        <div className="header__container-description">
+          <span>main title</span>
+          <span>Related Posts</span>
+          <ul>
+            <li>Tourism</li>
+            <li>Photograpy</li>
+            <li>Travel News</li>
+          </ul>
+        </div>
+        <header className="header__container-title">
+          <h1>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum
+            ducimus facilis libero nisi.
+          </h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis
+            praesentium placeat magni nam reprehenderit, alias enim dolorum
+            delectus reiciendis totam labore, voluptate officia accusamus
+            doloribus unde iste repellat. Molestias, eos.
+          </p>
+        </header>
+      </div>
+      <div className="slider__container">
+        <Image src={artibleImg} alt="arible image" />
+      </div>
+    </article>
+  );
 };
 
 export default Hero;
